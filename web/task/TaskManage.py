4860b12ae205e364e4cf438599f2e726e86dc626
x <294797097@qq.com>
2017-12-12 01:19:17 +0800

    重新定义时间

--- web/task/TaskManage.py
+++ web/task/TaskManage.py
@@ -36,7 +36,7 @@ def getCurPlateLastData():
 
 
 def saveStockNewData():
-    if  Holiday.get_cur_date():
+    if Holiday.get_cur_date():
         logging.info("开始保存当前new stock 数据")
         StockService().saveStockNew()
 
@@ -66,13 +66,13 @@ def checkPublicNewStockStatus():
 
 def commonTask():
     # 获取当前stock daily数据
-    schedudler.add_job(getCurStockDailyData, 'cron', minute='42', hour='18', day_of_week='0-4')
+    schedudler.add_job(getCurStockDailyData, 'cron', minute='10', hour='15', day_of_week='0-4')
     # 获取当前stock last 数据
-    schedudler.add_job(getCurStockLastData, 'cron', minute='25', hour='15', day_of_week='0-4')
+    schedudler.add_job(getCurStockLastData, 'cron', minute='30', hour='18', day_of_week='0-4')
     # 获取当前plate daily 数据
-    schedudler.add_job(getCurPlateDailyData, 'cron', minute='10', hour='17', day_of_week='0-4')
+    schedudler.add_job(getCurPlateDailyData, 'cron', minute='30', hour='16', day_of_week='0-4')
     # 获取当前plate last 数据
-    schedudler.add_job(getCurPlateLastData, 'cron', minute='45', hour='16', day_of_week='0-4')
+    schedudler.add_job(getCurPlateLastData, 'cron', minute='30', hour='17', day_of_week='0-4')
 
 
 def start():
@@ -81,16 +81,12 @@ def start():
     schedudler.add_job(updateStockBlock, 'cron', minute='00', hour='8', day_of_week='0-4')
 
     # 保存新股数据
-    schedudler.add_job(saveStockNewData, 'cron', minute='30', hour='16', day_of_week='0-4')
+    schedudler.add_job(saveStockNewData, 'cron', minute='30', hour='23', day_of_week='0-4')
 
     # 保存新股数据
-    schedudler.add_job(checkPublicNewStockStatus, 'cron', minute='*/1', hour='9-11', day_of_week='0-4')
-
+    schedudler.add_job(checkPublicNewStockStatus, 'cron', minute='*/5', hour='9-11', day_of_week='0-4')
 
     # 检测停牌数据
     schedudler.add_job(checkStopStock, 'cron', minute='32', hour='9', day_of_week='0-4')
-
-
-
 
     schedudler.start()
