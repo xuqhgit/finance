804343d1fb86b29d85b61c62896d91c32f25f88d
x <294797097@qq.com>
2018-01-02 23:41:36 +0800

    优化

--- web/utils/Holiday.py
+++ web/utils/Holiday.py
@@ -18,6 +18,10 @@ def get_cur_date():
     return time.strftime('%Y%m%d', time.localtime(time.time()))
 
 
+def get_cur_year():
+    return time.strftime('%Y', time.localtime(time.time()))
+
+
 def is_trade_time():
     """
     是否是交易时间 其中已 925 竞价结束为开始时间
