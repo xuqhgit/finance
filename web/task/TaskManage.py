28b56ed73db521f63f9477eb6a4cbd879fdb043f
x <294797097@qq.com>
2018-01-22 22:58:58 +0800

    优化

--- web/task/TaskManage.py
+++ web/task/TaskManage.py
@@ -14,19 +14,19 @@ schedudler = BackgroundScheduler(daemonic=False)
 
 # url = "http://qt.gtimg.cn/r=0.4244364923797548q=marketStat,stdunixtime"
 def getCurStockDailyData():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始获取当前stock daily数据")
         StockService().saveAllDailyStocks()
 
 
 def getCurStockLastData():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始获取当前stock last 数据")
         StockService().saveAllDailyStocksLast()
 
 
 def getCurPlateDailyData():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始当前plate daily 数据")
         StockService().saveAllDailyPlate()
 
@@ -38,25 +38,25 @@ def getCurPlateLastData():
 
 
 def saveStockNewData():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始保存当前new stock 数据")
         StockService().saveStockNew()
 
 
 def updateStockBlock():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始更新stock block 数据")
         StockService().saveStockBlockData()
 
 
 def checkStopStock():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         logging.info("开始检查 stop stock  数据")
         StockService().checkStopStockCode()
 
 
 def checkPublicNewStockStatus():
-    if Holiday.get_cur_date():
+    if Holiday.is_trade_date():
         cur_time = time.strftime('%H%M', time.localtime(time.time()))
         cur_time = int(cur_time)
         if 930 < cur_time < 1130 or 1300 < cur_time < 1500:
@@ -71,11 +71,11 @@ def commonTask():
     # 获取当前stock daily数据
     schedudler.add_job(getCurStockDailyData, 'cron', minute='10', hour='15', day_of_week='0-4')
     # 获取当前stock last 数据
-    schedudler.add_job(getCurStockLastData, 'cron', minute='30', hour='18', day_of_week='0-4')
+    schedudler.add_job(getCurStockLastData, 'cron', minute='30', hour='16', day_of_week='0-4')
     # 获取当前plate daily 数据
-    schedudler.add_job(getCurPlateDailyData, 'cron', minute='30', hour='16', day_of_week='0-4')
+    schedudler.add_job(getCurPlateDailyData, 'cron', minute='00', hour='16', day_of_week='0-4')
     # 获取当前plate last 数据
-    schedudler.add_job(getCurPlateLastData, 'cron', minute='30', hour='17', day_of_week='0-4')
+    schedudler.add_job(getCurPlateLastData, 'cron', minute='15', hour='16', day_of_week='0-4')
 
     # 更新事件
     schedudler.add_job(update_all_stock_event, 'cron', minute='00', hour='9', day_of_week='0-4')
@@ -88,7 +88,7 @@ def start():
     schedudler.add_job(updateStockBlock, 'cron', minute='00', hour='8', day_of_week='0-4')
 
     # 保存新股数据
-    schedudler.add_job(saveStockNewData, 'cron', minute='30', hour='23', day_of_week='0-4')
+    schedudler.add_job(saveStockNewData, 'cron', minute='00', hour='18', day_of_week='0-4')
 
     # 检测新股数据
     schedudler.add_job(checkPublicNewStockStatus, 'cron', minute='*/5', hour='9-11', day_of_week='0-4')
